b653672680cd11797a6834aa09e789962e918ea0
Santiago <santiago.castillo.silva211103@gmail.com>
2025-09-27 17:20:05 -0500

    Maximizar pantalla

--- SistemaPOS/src/main/java/pos/App.java
+++ SistemaPOS/src/main/java/pos/App.java
@@ -2,15 +2,19 @@ package pos;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/pos/MainView.fxml")), 1200, 720);
-        stage.setTitle("POS JavaFX");
+        Parent root = FXMLLoader.load(getClass().getResource("/pos/MainView.fxml"));
+        //Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/pos/MainView.fxml")), 1600, 800);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("POS JavaFX");
+        stage.setMaximized(true);
         stage.show();
     }
     public static void main(String[] args) { launch(args); }
